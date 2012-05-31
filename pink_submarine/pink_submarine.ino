4261f798e4643a40d4b417450a461a72c5df5b8c
Adam Perry-Pelletier <adam.perrypelletier@gmail.com>
2012-05-30 18:45:13 -0700

    Removed dead commented code.

--- pink_submarine/pink_submarine.ino
+++ pink_submarine/pink_submarine.ino
@@ -15,37 +15,6 @@ void setup() {
 }
 
 void loop() {
-  /*
-  pinkSubmarine.forward();
-  delay(1000);
-  pinkSubmarine.stopROV();
-  delay(1000);  
-  
-  pinkSubmarine.reverse();
-  delay(1000);
-  pinkSubmarine.stopROV();
-  delay(1000);
-  
-  pinkSubmarine.rotateLeft();
-  delay(1000);
-  pinkSubmarine.stopROV();
-  delay(1000);
- 
-  pinkSubmarine.rotateRight();
-  delay(1000);
-  pinkSubmarine.stopROV();
-  delay(1000);
-  
-  pinkSubmarine.up();
-  delay(1000);
-  pinkSubmarine.stopROV();
-  delay(1000);
-  
-  pinkSubmarine.down();
-  delay(1000);
-  pinkSubmarine.stopROV();
-  delay(3000);
-  */
   delay(500);
   chuck.update();
   pinkSubmarine.receiveWiiChuckXY(chuck.readJoyX(), chuck.readJoyY());
